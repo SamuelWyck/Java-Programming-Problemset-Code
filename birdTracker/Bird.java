948da12daef88488699092c65d3a968acbb036ca
SamuelWyck <samawyck@gmail.com>
2024-11-01 09:25:58 -0500

    Create Bird.java

---
+++ birdTracker/Bird.java
@@ -0,0 +1,31 @@
+public class Bird {
+    private String name;
+    private String latinName;
+    private int observations;
+    
+    public Bird(String name, String latinName) {
+        this.name = name;
+        this.latinName = latinName;
+        this.observations = 0;
+    }
+    
+    public String toString() {
+        return this.name + "(" + this.latinName + "): " + this.observations + " observations";
+    }
+    
+    public void addObservation() {
+        this.observations += 1;
+    }
+    
+    public String getName() {
+        return this.name;
+    }
+    
+    public String getLatinName() {
+        return this.latinName;
+    }
+    
+    public int getObservations() {
+        return this.observations;
+    }
+}
